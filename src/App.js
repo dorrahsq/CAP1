ca09174ceb7fe8d0fdd31471630e0309ec34320e
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-03 16:57:13 +0300

    task

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+<h1>Welcome to the flip card game</h1>
+<button> Start game  </button>
+<div></div>
+<button> instruction </button>
+    </div>
+  );
+}
+
+export default App;
