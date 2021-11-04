c7400432489689fa7f4d16577b6f04e1039269a0
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-04 15:16:19 +0300

    proj

--- src/components/cards/index.js
+++ src/components/cards/index.js
@@ -8,68 +8,60 @@ const Cards = () => {
   const [Cards, setCards] = useState([
     {
       id: 1,
-      name:"a",
+      name: "a",
       isflip: false,
-      ismatch: false ,
+      ismatch: false,
       img: "https://i.pinimg.com/564x/53/b2/d6/53b2d6ccc3e2a9c789bcc867ab55cb6c.jpg",
-      
     },
     {
       id: 2,
-      name:"b",
+      name: "b",
       isflip: false,
-      ismatch: false ,
+      ismatch: false,
       img: "https://i.pinimg.com/564x/d5/4c/d1/d54cd1fef144248fdedfede82f2261b4.jpg",
-      
     },
     {
       id: 3,
-      name:"c",
+      name: "c",
       isflip: false,
-      ismatch: false ,
+      ismatch: false,
       img: "https://i.pinimg.com/564x/10/8f/c0/108fc03650cd0c8347ec81c172acd105.jpg",
-      
     },
     {
       id: 4,
-      name:"d",
+      name: "d",
       isflip: false,
-      ismatch: false ,
+      ismatch: false,
       img: "https://i.pinimg.com/564x/9f/29/a1/9f29a1ae09a7f9a8c10afe9d297a23e8.jpg",
-      
     },
     {
-        id: 5,
-        name:"a",
-        isflip: false,
-      ismatch: false ,
-        img: "https://i.pinimg.com/564x/53/b2/d6/53b2d6ccc3e2a9c789bcc867ab55cb6c.jpg",
-        
-      },
-      {
-        id: 6,
-        name:"b",
-        isflip: false,
-        ismatch: false ,
-        img: "https://i.pinimg.com/564x/d5/4c/d1/d54cd1fef144248fdedfede82f2261b4.jpg",
-       
-      },
-      {
-        id: 7,
-        name:"c",
-        isflip: false,
-        ismatch: false ,
-        img: "https://i.pinimg.com/564x/10/8f/c0/108fc03650cd0c8347ec81c172acd105.jpg",
-        
-      },
-      {
-        id: 8,
-        name:"d",
-        isflip: false,
-        ismatch: false ,
-        img: "https://i.pinimg.com/564x/9f/29/a1/9f29a1ae09a7f9a8c10afe9d297a23e8.jpg",
-        
-      },
+      id: 5,
+      name: "a",
+      isflip: false,
+      ismatch: false,
+      img: "https://i.pinimg.com/564x/53/b2/d6/53b2d6ccc3e2a9c789bcc867ab55cb6c.jpg",
+    },
+    {
+      id: 6,
+      name: "b",
+      isflip: false,
+      ismatch: false,
+      img: "https://i.pinimg.com/564x/d5/4c/d1/d54cd1fef144248fdedfede82f2261b4.jpg",
+    },
+    {
+      id: 7,
+      name: "c",
+      isflip: false,
+      ismatch: false,
+      img: "https://i.pinimg.com/564x/10/8f/c0/108fc03650cd0c8347ec81c172acd105.jpg",
+    },
+    {
+      id: 8,
+      name: "d",
+      isflip: false,
+      ismatch: false,
+      img: "https://i.pinimg.com/564x/9f/29/a1/9f29a1ae09a7f9a8c10afe9d297a23e8.jpg",
+    },
   ]);
   const [Firstchoice, setFirstchoice] = useState(null);
   const [Secondchoice, setSecondchoice] = useState(null);
@@ -101,36 +93,40 @@ const Cards = () => {
   };
 
   let count = 0;
-  const handleClick = (item , i) => {
+  const handleClick = (item, i) => {
     // console.log(item);
     if (Firstchoice === null) {
-        
       setFirstchoice(item);
-      setcardtry(cardtry.map((ele)=>{
-          if (ele.id === item.id){
-              return {...ele , isflip:true}
-          }else {
-              return ele
+      setcardtry(
+        cardtry.map((ele) => {
+          if (ele.id === item.id) {
+            return { ...ele, isflip: true };
+          } else {
+            return ele;
           }
-      }))
-    
+        })
+      );
 
-   /////herjh
+      /////herjh
       count++;
     } else {
-console.log("item", item);
-console.log("Firstchoice",Firstchoice);
+      console.log("item", item);
+      console.log("Firstchoice", Firstchoice);
       if (item.name === Firstchoice.name) {
         console.log("same");
-        setcardtry(cardtry.map((ele)=>{
-            if (ele.name === Firstchoice.name){
-                // setcardtry(ele.isflip = true )
-                return {...ele , ismatch:true , isflip:true}
-            }else {
-                // setcardtry(ele.isflip = false )
-                return  ele
+        setcardtry(
+          cardtry.map((ele) => {
+            if (ele.name === Firstchoice.name) {
+              // setcardtry(ele.isflip = true )
+              return { ...ele, ismatch: true, isflip: true };
+            } else {
+              // setcardtry(ele.isflip = false )
+              return ele;
+
+              /////////
             }
-        }))
+          })
+        );
 
         // setcardtry([...cardtry , item.isflip= true])
 
@@ -138,13 +134,23 @@ console.log("Firstchoice",Firstchoice);
         count = 0;
         setFirstchoice(null);
       } else {
+        setcardtry(
+          cardtry.map((ele) => {
+            if (ele.ismatch) {
+              return ele;
+            } else {
+              return { ...ele, isflip: false };
+            }
+          })
+        );
+
+        /////
 
         // setcardtry([...cardtry , item.isflip= false])
 
         console.log("diff");
         count = 0;
         setFirstchoice(null);
-        
       }
     }
   };
@@ -152,20 +158,39 @@ console.log("Firstchoice",Firstchoice);
   return (
     <div className="allCards">
       {cardtry.map((item, i) => {
-          {console.log(item);}
-        return (
-          <div className={item.isflip? "flipped" : ""} onClick={() => flipCard(item.id)}>
-            <img className="front" src={item.img} />
-            <img
-              className="back"
-              onClick={() => handleClick(item , i)}
-              src={backimge}
-            />
-          </div>
-        );
+        if (item.isflip) {
+          return (
+            <div>
+              {" "}
+              <img className="front" src={item.img} />{" "}
+            </div>
+          );
+        } else {
+          return (
+            <div>
+              {" "}
+              <img
+                className="back"
+                onClick={() => handleClick(item, i)}
+                src={backimge}
+              />{" "}
+            </div>
+          );
+        }
+        // return (
+        //   <div className={item.isflip? "flipped" : ""} onClick={() => flipCard(item.id)}>
+        //     <img className="front" src={item.img} />
+        //     <img
+        //       className="back"
+        //       onClick={() => handleClick(item , i)}
+        //       src={backimge}
+        //     />
+        //   </div>
+        // );
       })}
+      {console.log(cardtry)}
     </div>
   );
-};
+}; ///////
 
 export default Cards;
